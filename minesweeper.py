316effb82534e7af57f3731ec29854733bbe4086
SRCinVA <scitarella@hotmail.com>
2021-04-24 21:04:39 -0400

    First commit
    Started to build board

---
+++ minesweeper.py
@@ -0,0 +1,15 @@
+class Board:    # this creates the board object
+    def __init__(self, dim_size, num_bombs):  
+        # need to track these parameters:
+        self.dim_size = dim_size
+        self.num_bombs = num_bombs
+
+def play(dim_size=10, num_bombs=10):
+    # create board and plant bombs
+    # show the user the board and ak them for input
+    # if location is a bomb, the game ends
+    # if not, dig recurvsively until until the spot is next to a bomb
+    # rinse/repeat until there are no more spots to dig
+
+    pass
+
