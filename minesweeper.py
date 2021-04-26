07a03e57325f9e157714109294492d4d66317369
SRCinVA <scitarella@hotmail.com>
2021-04-26 13:59:52 -0400

    tracked rows and columns
    Don't understand reasoning behind determining rows and columns.

--- minesweeper.py
+++ minesweeper.py
@@ -1,3 +1,5 @@
+import random
+
 class Board:    # this creates the board object
     def __init__(self, dim_size, num_bombs):  
         # need to track these parameters:
@@ -17,7 +19,17 @@ class Board:    # this creates the board object
                                 # in the form of a list of lists (good for a 2d structure)
         board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
         # it will be a square, so both of those blanks will be the same number
-        # (seems to be columns to rows.)
+        # (seems to be columns and then rows.)
+
+        # plant the bombs:
+        bombs_planted = 0
+        while bombs_planted < self.num_bombs:
+            loc = random.randint(0, self.dim_size**2 - 1) # **2 to square it; - 1 so that it encompasses the highest index.
+            row = loc // self.dim_size  # don't understand this. (Space 22 is not in row 4.)
+            col = loc % self.dim_size # don't understand this one, either
+
+            if board[row][col] == '*': # if that spot equals a bomb (the star) ...
+                
 
 def play(dim_size=10, num_bombs=10):
     # create board and plant bombs
