6d5d17ca87f12be1eb74294611dfea5046658b4d
SRCinVA <scitarella@hotmail.com>
2021-04-27 16:32:48 -0400

    challenging concept today ...

--- minesweeper.py
+++ minesweeper.py
@@ -8,12 +8,19 @@ class Board:    # this creates the board object
 
         # let's create the board, first with a helper function:
         self.board = self.make_new_board() # this will enable us to plant the bombs
-
+        # my understanding on why we're doing this ... is not clear
+        self.assign_values_to_board()
 
         # intialize a set to keep track of which locations have been uncovered.
         # (this will be in tuples of row, column)
 
         self.dug = set()
+
+    def assign_values_to_board(self):
+        # !?!?! with bombs planted, assign a number of 0-8 to all empty spaces,
+        # which indicates how many neighboring bombs there are. (How will this help us ... ?)
+        for r in range(self.dim_size):
+            
 
     def make_new_board(self):   # using dim size and num_bombs, make a new board
                                 # in the form of a list of lists (good for a 2d structure)
@@ -23,13 +30,18 @@ class Board:    # this creates the board object
 
         # plant the bombs:
         bombs_planted = 0
-        while bombs_planted < self.num_bombs:
+        while bombs_planted < self.num_bombs: # we're doing something unbounded, so a while loop is better
             loc = random.randint(0, self.dim_size**2 - 1) # **2 to square it; - 1 so that it encompasses the highest index.
             row = loc // self.dim_size  # don't understand this. (Space 22 is not in row 4.)
             col = loc % self.dim_size # don't understand this one, either
 
-            if board[row][col] == '*': # if that spot equals a bomb (the star) ...
-                
+            if board[row][col] == '*': # if that spot equals a bomb (the star), then we've alrady planted one.
+                continue # meaning, just keep going on
+
+            board[row][col] = '*'
+            bombs_planted += 1
+        # when all of this is done, return the board:
+        return board
 
 def play(dim_size=10, num_bombs=10):
     # create board and plant bombs
